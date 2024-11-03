eb4f5497fc182e672070997a784eca1f4bf4a970
Liabø <herman.liabo@sweco.no>
2024-11-03 14:47:38 +0100

    initial-commit

---
+++ src/classes/Project.ts
@@ -0,0 +1,76 @@
+import { v4 as uuidv4} from "uuid"
+
+export type ProjectStatus = "pending" | "active" | "finished"
+export type UserRole = "architect" | "engineer" | "developer"
+
+
+export interface IProject {
+  name: string
+  description: string
+  status: ProjectStatus
+  userRole: UserRole
+  finishDate: Date
+}
+
+export class Project implements IProject{
+  //
+  name: string
+  description: string
+  status: "pending" | "active" | "finished"
+  userRole: "architect" | "engineer" | "developer"
+  finishDate: Date
+
+  // Class internals
+  ui: HTMLDivElement
+  cost: number = 0
+  progress: number = 0
+  id: string
+
+  constructor(data: IProject) {
+    //Project data definition
+
+    for (const key in data) {
+      this[key] = data[key]
+    }
+
+    this.id = uuidv4()
+
+    //Set project card UI
+    this.setUI()
+  }
+
+  setUI() {
+    
+    if (this.ui) {return}
+
+    this.ui = document.createElement("div")
+    this.ui.className = "project-card"
+    this.ui.innerHTML = `
+    <div class="card-header">
+      <p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1;">HC</p>
+      <div>
+        <h5>${this.name}</h5>
+        <p>${this.description}</p>
+      </div>
+    </div>
+    <div class="card-content">
+      <div class="card-property">
+        <p style="color: #969696;">Status</p>
+        <p>${this.status}</p>
+      </div>
+      <div class="card-property">
+        <p style="color: #969696;">Role</p>
+        <p>${this.userRole}</p>
+      </div>
+      <div class="card-property">
+        <p style="color: #969696;">Cost</p>
+        <p>$${this.cost}</p>
+      </div>
+      <div class="card-property">
+        <p style="color: #969696;">Estimated Progress</p>
+        <p>${this.progress * 100}%</p>
+      </div>
+    </div>
+    `
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import { IProject, ProjectStatus, UserRole } from "./classes/Project"
+import { ProjectsManager } from "./classes/ProjectsManager"
+
+function toggleModal(id: string, show: boolean) {
+  const modal = document.getElementById(id)
+  if (modal && modal instanceof HTMLDialogElement) {
+    if (show) {
+      modal.showModal()
+    }
+    else {
+      modal.close()
+    }
+  } else {
+    console.warn("The element id was not found in the document.")
+  }
+  
+}
+
+const projectsListUI = document.getElementById("projects-list") as HTMLElement
+const projectsManager = new ProjectsManager(projectsListUI)
+
+/* This document object is provided by the browser, 
+and its main purpose is to help us interact with the DOM. */
+const newProjectBtn = document.getElementById("new-project-btn")
+
+if (newProjectBtn) {
+  newProjectBtn.addEventListener("click", () => {toggleModal("new-project-modal", true)})
+} else {
+  console.warn("New project button was not found.")
+}
+
+
+const projectForm = document.getElementById("new-project-form")
+if (projectForm && projectForm instanceof HTMLFormElement) {
+  projectForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formData = new FormData(projectForm)
+    const projectData: IProject = {
+      name: formData.get("name") as string,
+      description: formData.get("description") as string,
+      status: formData.get("status") as ProjectStatus,
+      userRole: formData.get("userRole") as UserRole,
+      finishDate: new Date(formData.get("finishDate") as string)
+    }
+    try {
+      const project = projectsManager.newProject(projectData)
+      projectForm.reset()
+      toggleModal("new-project-modal", false)
+    } catch (error) {
+      alert(error)
+    }
+    
+  })
+  const cancelProjectFormBtn = document.getElementById("cancel-project-form-button")
+  if (cancelProjectFormBtn) {
+    cancelProjectFormBtn.addEventListener("click", () => {
+      projectForm.reset()
+      toggleModal("new-project-modal", false)})
+  } else {
+    console.warn("Cancel project form button was not found.")
+  }
+} else {
+  console.warn("The project form was not found. Check the ID!")
+}
+
+const exportProjectsBtn = document.getElementById("export-projects-btn")
+if (exportProjectsBtn) {
+  exportProjectsBtn.addEventListener("click", () => {
+    projectsManager.exportToJSON()
+  })
+}
+
+const importProjectsBtn = document.getElementById("import-projects-btn")
+if (importProjectsBtn) {
+  importProjectsBtn.addEventListener("click", () => {
+    projectsManager.importFromJSON()
+  })
+}
+
+const projectsPage = document.getElementById("projects-page");
+const sidebarProjectsButton = document.getElementById("sidebar-projects-btn");
+
+if (sidebarProjectsButton && projectsPage) {
+  sidebarProjectsButton.addEventListener("click", () => {
+    // Show projects page and hide others if necessary
+    projectsPage.style.display = "flex";
+    const detailsPage = document.getElementById("project-details");
+    const usersPage = document.getElementById("users-page");
+
+    if (detailsPage) detailsPage.style.display = "none";
+    if (usersPage) usersPage.style.display = "none";
+  });
+}
